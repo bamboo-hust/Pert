b65cbcff9444d0c941d4b2a9e2040ada0878d796
tungtt <tungtt@coccoc.com>
2021-01-26 16:12:40 +0700

    fixed drawer.py

--- drawer.py
+++ drawer.py
@@ -37,23 +37,33 @@ class AnnoteFinder:  # thanks to http://www.scipy.org/Cookbook/Matplotlib/Intera
                         dx, dy = x - clickX, y - clickY
                         self.drawAnnote(event.inaxes, x, y, a)
     def drawAnnote(self, axis, x, y, annote):
-        if (dist_sum[annote] == 0):
-            fig1, ax1 = plt.subplots()
-            ax1.set_title('Module')
-            ax1.plot(dist_all[annote]);
+        list_node = list(G.nodes())
+        index = list_node.index(annote)
+        if (dist_sum[index] == 0):
+            fig1, ax = plt.subplots()
+            ax.set_title('Module ' + str(annote) + ' : Completion time distribution')
+            ax.plot(dist_all[index])
+            ax.set_xlabel('Time ')
+            ax.set_ylabel('Probability')
         else:
             fig1, ax = plt.subplots(2)
-            ax[0].set_title('Sprint')
-            ax[0].plot(dist_all[annote]);
-            ax[1].set_title('Cumulative sprint')
-            ax[1].plot(dist_sum[annote]);
+            ax[0].set_title('Sprint ' + str(annote) + ' : Completion time distribution')
+            ax[0].plot(dist_all[index])
+            ax[0].set_xlabel('Time')
+            ax[0].set_ylabel('Probability')
+            ax[1].set_title('Completion time from start to end of Sprint ' + str(annote) + ' :');
+            ax[1].plot(dist_sum[index])
+            ax[1].set_xlabel('Time')
+            ax[1].set_ylabel('Probability')
+            plt.tight_layout()
         plt.show();
 
 dist_all = [0] * 17
 dist_sum = [0] * 17
+G = nx.Graph()
 
 def draw_origin_graph():
-    global distribution
+    global distribution, G
     G = read_graph('module_relations.txt')
     color_map = ['black'] * 16
     for i in range(1, 13):
